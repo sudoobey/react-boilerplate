885c39f1a6f5ced19ee6dbe8e36a8f1587c98a63
Aleksandr Plesovskikh <me@hgen.ru>
2016-06-09 16:40:26 +0500

    init

---
+++ server/server.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const requireDir = require('require-dir');
+
+const Koa = require('koa');
+const Router = require('koa-router');
+const mount = require('koa-mount');
+const app = new Koa();
+
+// body parser
+const bodyParser = require('koa-bodyparser');
+app.use(bodyParser());
+
+// parse qs
+require('koa-qs')(app);
+
+// logger
+const logger = require('koa-logger');
+app.use(logger());
+
+// trust proxy
+app.proxy = true;
+
+// Routre controllers
+const controllers = requireDir('./server/controllers');
+let apiRouter = new Router({prefix: '/api'});
+for (let name of Object.keys(controllers)) {
+    let controller = controllers[name];
+    controller.prefix(`/${name}`);
+    apiRouter.use('', controller.routes(), controller.allowedMethods());
+}
+app.use(apiRouter.routes(), apiRouter.allowedMethods());
+
+// serve static
+let staticRoute = new Router();
+let serve = require('koa-static');
+app.use(staticRoute.routes(), staticRoute.allowedMethods());
+app.use(mount(
+    '/node_modules',
+    serve(path.join(__dirname, '/node_modules'))
+));
+app.use(serve(path.join(__dirname, '/client')), {defer: true});
+
+// start server
+if (!module.parent) {
+    app.listen(
+        5555,
+        '0.0.0.0',
+        () => console.log('Server listening on 5555')
+    );
+}
