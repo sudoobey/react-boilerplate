232ca7ec65ff2a3f05e471e2f57098c63932bfb3
Александр Плесовских <me@hgen.ru>
2016-09-21 19:47:56 +0500

    menu

--- view/components/Menu/Menu.jsx
+++ view/components/Menu/Menu.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import {Link} from 'react-router';
 import styles from './Menu.css';
 
+// import {MorphReplace} from 'react-svg-morph';
+import IconMenu from 'react-icons/lib/md/menu';
+import IconClose from 'react-icons/lib/md/close';
+
 import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
@@ -18,13 +22,16 @@ export default class App extends React.Component {
     }
 
     render() {
+        let active = this.state.active;
+        let toggle = this.toggleMenu.bind(this);
         return (
             <nav>
-                <div className={cx('nav', {'nav--active': this.state.active})}>
+                <div className={cx('nav', {'nav--active': active})}>
                     <Link className={styles.nav__link} to="#">wtf</Link>
                 </div>
-                <div className={styles.nav_button}
-                    onClick={() => this.toggleMenu()}></div>
+                <div className={styles.nav_button} onClick={toggle}>
+                    {active ? <IconMenu size={50}/> : <IconClose size={50}/>}
+                </div>
             </nav>
         );
     }
