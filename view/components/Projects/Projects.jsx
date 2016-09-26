6bf8a1a659efd39b5c12c5005df272ecb73d68dc
Александр Плесовских <me@hgen.ru>
2016-09-26 17:05:33 +0500

    fix link in project block

--- view/components/Projects/Projects.jsx
+++ view/components/Projects/Projects.jsx
@@ -1,6 +1,8 @@
 import {Link} from 'react-router';
+import classNames from 'classnames/bind';
 import Project from '../Project/Project.jsx';
 import styles from './Projects.css';
+const cx = classNames.bind(styles);
 
 const PROJECTS = ['blue', 'red', 'green', 'orange'];
 
@@ -8,17 +10,23 @@ function Projects(props) {
     let contentFor = props.children ? props.children.type.for : null;
     return (
         <div>
-        {PROJECTS.map((projectName) =>
-            <Link
-                className={styles.projects__item}
-                to={`/projects/${projectName}`}
-                key={projectName}
-            >
+        {PROJECTS.map((projectName) => {
+            let match = projectName === contentFor;
+            return (
+            <div>
+                <Link
+                    className={cx('projects__link', {
+                        'projects__link--current': match
+                    })}
+                    to={`/projects/${projectName}`}
+                    key={projectName}
+                />
                 <Project style={{background: projectName}}>
-                    {projectName === contentFor ? props.children : null}
+                    {match ? props.children : null}
                 </Project>
-            </Link>
-        )}
+            </div>
+            );
+        })}
         </div>
     );
 }
